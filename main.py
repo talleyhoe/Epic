344d9d718e0123e2fa12b450a5c9e89f1f83e34b
cincottash <cincotta@udel.edu>
2022-11-24 00:31:30 -0500

    v1 done

--- main.py
+++ main.py
@@ -1,30 +1,67 @@
-from setup import *
 import requests
-import os.path
 import subprocess as sp
+from setup import *
 #get the file name using the api
 #use wget to download the image
+#iterate over all the returned images to find one that matches our input date
+	
+def downloadImages(rootSavePath):
+	dateResponse = requests.get('https://epic.gsfc.nasa.gov/api/enhanced/all')
+	dateList = []
+	for date in dateResponse.json():
+		dateList.append(date['date'])
+
+	if not os.path.isdir(rootSavePath):
+		print(f'Creating path {rootSavePath}\n')
+		sp.run(['mkdir', rootSavePath])
+	else:
+		print(f'Found existing directory for {rootSavePath}\n')
+
+	for date in dateList:
+		savePath = os.path.join(rootSavePath, date[0:4], date[5:7], date[8:10])
+		#check if folder exists for year
+		if not os.path.isdir(os.path.join(rootSavePath, date[0:4])):
+				print(f'Creating path {os.path.join(rootSavePath, date[0:4])}\n')
+				sp.run(['mkdir', os.path.join(rootSavePath, date[0:4])])
+		else:
+			print(f'Found existing directory for {os.path.join(rootSavePath, date[0:4])}\n')
+
+		#check is year/month folder exists
+		if not os.path.isdir(os.path.join(rootSavePath, date[0:4], date[5:7])):
+				print(f'Creating path {os.path.join(rootSavePath, date[0:4], date[5:7])}\n')
+				sp.run(['mkdir', os.path.join(rootSavePath, date[0:4], date[5:7])])
+		else:
+			print(f'Found existing directory for {os.path.join(rootSavePath, date[0:4], date[5:7])}\n')
+
+		#check if year/month/day folder exists
+		if not os.path.isdir(os.path.join(rootSavePath, date[0:4], date[5:7], date[8:10])):
+				print(f'Creating path {os.path.join(rootSavePath, date[0:4], date[5:7], date[8:10])}\n')
+				sp.run(['mkdir', os.path.join(rootSavePath, date[0:4], date[5:7], date[8:10])])
+		else:
+			print(f'Found existing directory for {os.path.join(rootSavePath, date[0:4], date[5:7], date[8:10])}, skipping this day\n')
+			continue
+
+		imageNameResponseJson = requests.get(f'https://epic.gsfc.nasa.gov/api/natural/date/{date}').json()
+
+		#iterating over every photo taken on the supplied day
+		for imageNum, response in enumerate(imageNameResponseJson):
+			savePath = os.path.join(rootSavePath, date[0:4], date[5:7], date[8:10])
+			imageName = response['image']
+			fullDate = response['date'].replace(' ', '_').replace('-', '_').replace(':', '_')
+
+			URL = f'https://api.nasa.gov/EPIC/archive/natural/{date[0:4]}/{date[5:7]}/{date[8:10]}/png/{imageName}.png?api_key=BkFVBBcoSre33RhWShgaYdDD1wyFowS3hUx7QmUb'
+			
+			savePath =  os.path.join(savePath, str(imageNum) + '.png')
+			cmd = f'curl -L -o {savePath} {URL}'
+			print(f'{cmd}\n')
+			sp.run(['curl', '-L', '-o', savePath, URL])	
+
+
+
 
 def main():
-	setup()
-	#print('hello world\n')
-	URL = 'https://epic.gsfc.nasa.gov/archive/natural/'
-	savePath = 'Images/'
-
-	response = requests.get('https://api.nasa.gov/EPIC/api/natural/images?api_key=BkFVBBcoSre33RhWShgaYdDD1wyFowS3hUx7QmUb')
-	jsonResponse = response.json()
-	
-	imageName = jsonResponse[0]['image']
-	date = jsonResponse[0]['date']
-	year = date[0:4]
-	month = date[5:7]
-	day = date[8:10]
-
-	URL +=  f'{year}/{month}/{day}/png/{imageName}.png'
-
-	cmd = f'curl -L -o {savePath+year+month+day} {URL}'
-	print(f'{cmd}\n')
-	sp.run(['curl', '-L', '-o', savePath+year+month+day, URL])
+	rootSavePath = setup()	
+	downloadImages(rootSavePath)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
+import sys
+import os.path
 def setup():
-	pass
+	rootSavePath = sys.argv[1]
+
+	if not os.path.isdir(rootSavePath):
+		print(f'{rootSavePath} does not exist')
+		exit(0)
+	
+
+	return rootSavePath
